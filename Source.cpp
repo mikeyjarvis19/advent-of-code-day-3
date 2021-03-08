30f9421d085ac7ec71400afcdebc209ff1da8e67
michael.jarvis <michael.jarvis@clinithink.com>
2021-03-08 15:01:37 +0000

    Solve part 2

--- Source.cpp
+++ Source.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-void part_1() {
+int countTreesHit(int moveRight, int moveDown) {
 	std::vector <std::vector<char>> all_rows;
 	std::ifstream fileContents("input.txt");
 	std::string rowString;
@@ -12,9 +12,9 @@ void part_1() {
 		all_rows.push_back(row);
 	}
 	auto numberOfRows = all_rows.size();
-	auto requiredCharWidth = (numberOfRows * 3) + 1;
+	auto requiredCharWidth = (numberOfRows * moveRight) + 1;
 	auto rowWidth = all_rows[0].size();
-	auto numberOfTimesToRepeatRows = requiredCharWidth/ rowWidth;
+	auto numberOfTimesToRepeatRows = requiredCharWidth / rowWidth;
 	for (int i = 0; i < all_rows.size(); i++) {
 		auto originalRow = all_rows[i];
 		for (int j = 0; j < numberOfTimesToRepeatRows; j++) {
@@ -28,16 +28,15 @@ void part_1() {
 	auto tree = '#';
 	auto treeMiss = 'O';
 	auto treeHit = 'X';
-	// MAKE CODE A FUNCTION FOR PART 2, y = y + down, x = x + right
-	for (int y = 0; y < all_rows.size(); y++) {
+	for (int y = 0; y < all_rows.size(); y+=moveDown) {
 		auto characterAtPosition = all_rows[y][x];
-		if (characterAtPosition == openSquare){
+		if (characterAtPosition == openSquare) {
 			all_rows[y][x] = treeMiss;
 		}
 		else if (characterAtPosition == tree) {
 			all_rows[y][x] = treeHit;
 		}
-		x = x + 3;
+		x += moveRight;
 	}
 	auto treeHitCount = 0;
 	auto rowsChecked = 0;
@@ -45,21 +44,31 @@ void part_1() {
 	for (int r = 0; r < all_rows.size(); r++) {
 		for (int c = 0; c < requiredCharWidth; c++) {
 			if (all_rows[r][c] == treeHit) {
-				//std::cout << "all_rows [r][c]: " << all_rows[r][c] << " ,tree: " << tree << "\n";
-				treeHitCount = treeHitCount + 1; 
+				treeHitCount = treeHitCount + 1;
 			}
 			charactersChecked++;
 		}
 		rowsChecked++;
-		int h = 1;
 	}
-	std::cout << "Rows checked: " << rowsChecked << "\n";
-	std::cout << "Characters checked: " << charactersChecked << "\n";
-	std::cout << "Trees hit: " << treeHitCount << "\n";
-	int i = 1;
+	return treeHitCount;
+}
+
+void part_1() {
+	auto part1hitcount = countTreesHit(3, 1);
+	std::cout << "Part 1 hits: " << part1hitcount << "\n";
+}
+
+void part_2() {
+	auto slope1count = countTreesHit(1, 1);
+	auto slope2count = countTreesHit(3, 1);
+	auto slope3count = countTreesHit(5, 1);
+	auto slope4count = countTreesHit(7, 1);
+	auto slope5count = countTreesHit(1, 2);
+	auto finalAnswer = slope1count * slope2count * slope3count * slope4count * slope5count;
+	std::cout << "Part 2 answer: " << finalAnswer << "\n";
 }
 
 int main() {
 	part_1();
-	//part_2();
+	part_2();
 }
